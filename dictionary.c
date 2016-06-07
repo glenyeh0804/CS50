3d527315c238bdfdd7dee9824fa5e8af1e8c70d0
glenyeh0804 <glenyeh0804@gmail.com>
2016-06-07 23:44:11 +0800

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -8,6 +8,7 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include "dictionary.h"
 #include <ctype.h>
@@ -36,31 +37,38 @@ bool check(const char* word)
     node* indicator = root;
     
     int i = 0;
+    int k = 0;
+    char transfer[LENGTH+1];
 
-    while (true)
+    while (word[i] != '\0')
     {
-        int k = word[i] - 97;
+        if (isalpha(word[i]))
+        {
+            transfer[i] = word[i];
+            transfer[i] = tolower(transfer[i]);
+            k = transfer[i] - 'a';
+        }
+        else if (word[i] == '\'')
+        {
+            k = 26;
+        }
+        
+        else 
+            return false;
         
         if (indicator->children[k] == NULL)
         {
             return false;
         }
-        else if (word[i] != '\0')
-        {
-            indicator = indicator->children[k];
-            i++;
-        }
-        else if (indicator->is_word)
-        {
-            return true;
-        }
         else
         {
-            return false;
+            indicator = indicator->children[k];
         }
+        i++;
     }
-    
 
+    return indicator->is_word;
+        
 }
 
 /**
@@ -74,65 +82,66 @@ bool load(const char* dictionary)
     if (dic == NULL)
     {
         printf("Could not open %s.\n", dictionary);
-        unload();
-        return 1;
+        return false;
     }
     
-
-    node* root = malloc(sizeof(node));
+    // Variables setting
+    root = malloc(sizeof(node));
+      if (root == NULL)
+        {
+            printf("Error allocating memory for root.\n");
+            fclose(dic);
+            return false;
+        }
     node* cursor = root;
+    int letter = 0;
     int index = 0;
+    char current_word[LENGTH+1];
     
     // retrieve the words in dictionary and put into tries
-    for (int c = fgetc(dic); c != EOF; c = fgetc(dic))
+    for (int chr = fgetc(dic); chr != EOF; chr = fgetc(dic))
     {
-        // allow only alphabetical characters
-        if (isalpha(c) || (c == '\'' && index > 0))
+        if (chr == '\n')
         {
-            if (cursor->children['c' - 97] == NULL)
+            for (int i = 0; i < index; i++)
             {
-                node* new_root = malloc(sizeof(node));
-                cursor->children['c' - 97] = new_root;
-                cursor = cursor->children['c' - 97];
-            }
-            else
-            {
-               cursor = cursor->children['c' - 97];
+                // allow only alphabetical characters
+                if (current_word[i] == '\0')
+                    {
+                         // Close a word
+                         cursor->is_word = true;
+                    }
+                
+                if (isalpha(current_word[i]))
+                {
+                    current_word[i] = tolower(current_word[i]); 
+                    letter = current_word[i] - 'a';
+                }
+                else if (current_word[i] == '\'')
+                {
+                    letter = 26; 
+                }
+
+                // setting the dicationary if NULL    
+                if (cursor->children[letter] == NULL)
+                    {
+                        cursor->children[letter] = malloc(sizeof(node));
+                    }
+                // point to next pointer
+                else 
+                    cursor = cursor->children[letter];
             }
             
+            // reset
+            index = 0;
+            
+        }
+        else
+        {
+            current_word[index] = chr;
             index++;
-            
-            // ignore alphabetical strings too long to be words
-            if (index > LENGTH)
-            {
-                // consume remainder of alphabetical string
-                while ((c = fgetc(dic)) != EOF && isalpha(c));
-
-                // prepare for new word
-                index = 0;
-            }
-        }
-
-        // ignore words with numbers (like MS Word can)
-        else if (isdigit(c))
-        {
-            // consume remainder of alphanumeric string
-            while ((c = fgetc(dic)) != EOF && isalnum(c));
-
-            // prepare for new word
-            index = 0;
-        }
-
-        // we must have found a whole word
-        else if (index > 0)
-        {
-            cursor->is_word = true;
-            cursor = root;
-            // prepare for next word
-            index = 0;
         }
     }
-    
     //close the file, finish the load
     fclose(dic);
     return true; 
